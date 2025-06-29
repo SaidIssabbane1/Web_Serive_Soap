80a5e6c0c1f7724444f43ecb8c74afca44e9ab9f
saidlionsgeeek <saidlionsgeek.1997@gmail.com>
2025-06-29 14:43:27 +0100

    end

---
+++ src/main/java/ws/Compte.java
@@ -0,0 +1,48 @@
+package ws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import java.util.Date;
+
+@XmlRootElement(name = "compte")//convert the objet Compte to objet en xml
+@XmlAccessorType(XmlAccessType.FIELD) // pour annottaion anote en dateCreation
+public class Compte {
+    private int code;
+    private double solde;
+    @XmlTransient //ignor cette attribut not getter et setter
+    private Date dateCreation;
+    public Compte(){
+
+    }
+    public Compte(int code, double solde, Date dateCreation) {
+        this.code = code;
+        this.solde = solde;
+        this.dateCreation = dateCreation;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    public void setSolde(double solde) {
+        this.solde = solde;
+    }
+
+    public Date getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+}
